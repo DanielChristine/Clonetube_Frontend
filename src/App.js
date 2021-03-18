429fab51ae249d0cca0e318b4a83f114fb79454b
Daniel Christine <danieljameschristine@gmail.com>
2021-03-17 21:15:43 -0500

    Added Navbar, created Comments form, created videoplayer file to simplify App.js

--- src/App.js
+++ src/App.js
@@ -1,27 +1,37 @@
 import React, { Component } from "react";
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./Navbar";
+import CommentForm from "./CommentForm";
 
 export default class App extends Component {
   constructor(props) {
     super(props);
-    this.state= {
-      mystate: []
-    }
+    this.state = {
+      mystate: [],
+    };
   }
 
   render() {
-  return (
-    <div className='main'>
-     
-     <iframe title= "myFrame"
-              id="ytplayer"
-              type="text/jsx"
-              width="800"
-              height="400"
-              src="https://www.youtube.com/embed/M7lc1UVf-VE?autoplay=1"
-              frameBorder="0"
-            ></iframe>
-
-    </div>
-  );
-}}
+    return (
+      <div className="main">
+        <div className="header">
+          <Navbar />
+        </div>
+        <div className="bigplayer">
+          <iframe
+            title="myFrame"
+            id="ytplayer"
+            type="text/jsx"
+            width="800"
+            height="400"
+            src="https://www.youtube.com/embed/M7lc1UVf-VE?autoplay=1"
+            frameBorder="none"
+          ></iframe>
+        </div>
+        <div className="commentFrame">
+          <CommentForm />
+        </div>
+      </div>
+    );
+  }
+}
---
+++ src/CommentForm.js
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import './App.scss';
+import Button from 'react-bootstrap/Button';
+
+class CommentForm extends Component {
+
+  render() {
+    return (
+      <div classname='CommentForm'>
+      <form>
+  <fieldset>
+    <legend>Legend</legend>
+    <div class="form-group row">
+      <label for="staticEmail" class="col-sm-2 col-form-label">Email</label>
+      <div class="col-sm-10">
+        <input type="text" readonly="" class="form-control-plaintext" id="staticEmail" value="email@example.com" />
+      </div>
+    </div>
+    <div class="form-group">
+      <label for="exampleInputEmail1">Email address</label>
+      <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
+      <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+    </div>
+    <div class="form-group">
+      <label for="exampleInputPassword1">Password</label>
+      <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" />
+    </div>
+    </fieldset>
+    </form></div>
+    );
+  }
+}
+export default CommentForm;
