1c3ac7d20afa6709b159ebfbcd7e7259c0d30bbf
cdevjarrell <christian.j.jarrell@gmail.com>
2021-03-24 16:15:07 -0400

    Refactor VPlayer component to display any video.

--- src/components/VPlayer.js
+++ src/components/VPlayer.js
@@ -1,21 +1,33 @@
 import React, { Component } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-class VPlayer extends Component {
-  render() {
+const VPlayer = ({ video }) => {
+  if (!video) {
     return (
-      <div className="bigplayer">
-        <iframe
-          title="myFrame"
-          id="ytplayer"
-          type="text/jsx"
-          width="800"
-          height="400"
-          src="https://www.youtube.com/embed/${this.props.videoid}?autoplay=0"
-          frameBorder="none"
-        ></iframe>
+      <div>
+        <h1>Enter search keyword to load</h1>
+        <br />
+        <p style={{ fontSize: "25px" }}>
+          Use the API to search for videos matching specific search terms,
+          publication dates, etc. The APIs search.list method also supports
+          searches for playlists and channels!!
+        </p>
       </div>
     );
   }
-}
+  const videoSrc = `https://www.youtube.com/embed/${video.id.videoId}`;
+  console.log(typeof video);
+  return (
+    <div>
+      <div className="ui embed">
+        <iframe src={videoSrc} allowFullScreen title="Video player" />
+      </div>
+      <div className="ui segment">
+        <h4 className="ui header">{video.snippet.title}</h4>
+        <p>{video.snippet.description}</p>
+      </div>
+    </div>
+  );
+};
+
 export default VPlayer;
